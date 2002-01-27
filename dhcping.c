865ab71ca4653f51d0d90fe51705f0f6448dc9ad
Edwin Groothuis <edwin@mavetju.org>
2002-01-27 01:57:15 +0000

    moved documentation into pod-format, removed origin

--- dhcping.c
+++ dhcping.c
@@ -1,10 +1,9 @@
 //
-// $Id: dhcping.c,v 1.2 2002/01/26 14:09:36 mavetju Exp $
+// $Id: dhcping.c,v 1.3 2002/01/27 01:57:15 mavetju Exp $
 //
 
 /*
- * Copyright (c) Atos-Origin MS GS Messaging
- * Author: Edwin Groothuis (Edwin.Groothuis@atosorigin.com)
+ * Copyright 2000, 2001, 2002 by Edwin Groothuis, edwin@mavetju.org
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -14,14 +13,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the AtosOrigin nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY ATOS-ORIGIN AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL ATOS-ORIGIN OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
